c3000aef960ef37697a27d05bfb9a3d775764e4e
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-26 22:39:55 +0100

    Set up Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mission from './components/Mission';
+import Rockets from './components/Rockets';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Navbar />
+      <div className="nav">
+        <Navbar />
+      </div>
+      <section className="content">
+        <Routes>
+          <Route path="/" exact element={<Rockets />} />
+          <Route path="/Mission"  element={<Mission />} />
+          <Route path="/My Profile" element={<MyProfile />} />
+        </Routes>
+      </section>
     </div>
+    </Router>
   );
 }
 
